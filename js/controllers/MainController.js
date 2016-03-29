7360fa9d32c4d877d08e287a384d81d673312ffd
modulor <modulor@hotmail.es>
2016-03-29 12:19:24 -0600

    ng-repeat

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -4,10 +4,19 @@ app.controller('MainController',['$scope',function($scope){
 	
 	$scope.promo = "Promo here";
 
-	$scope.product = {
-		name: 'The Book of Trees',
-		price: 19.99,
-		pubdate: new Date('2014', '03', '08')
-	}
+	$scope.products = [
+		{
+			name: 'The Book of Trees',
+			price: 19.99,
+			pubdate: new Date('2014', '03', '08'),
+			cover: 'img/book-of-trees.jpg'
+		},
+		{
+			name: 'Program or programed',
+			price: 9,
+			pubdate: new Date('2011', '01', '18'),
+			cover: 'img/program.jpg'
+		}
+	]
 
 }]);
